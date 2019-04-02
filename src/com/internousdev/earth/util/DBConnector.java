930a68b7ee9c285b667457fb275e3d1929d7558e
インターノウス <@DESKTOP-DFVO5UE>
2019-04-02 19:21:13 +0900

    DBConnector fixed

--- src/com/internousdev/earth/util/DBConnector.java
+++ src/com/internousdev/earth/util/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/jupiter?useSSL=false";
+	private static String url = "jdbc:mysql://localhost/earth?useSSL=false";
 	private static String user = "root";
 	private static String password = "mysql";
 
