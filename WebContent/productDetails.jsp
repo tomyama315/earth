f11008ed046aa70b4d4ad66e000756aaf3903ced
oreo3939 <iwajun3939@gmai.com>
2019-04-08 15:12:16 +0900

    4/8 productdetailsaction/jsp

--- WebContent/productDetails.jsp
+++ WebContent/productDetails.jsp
@@ -1,73 +1,90 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./css/style.css">
+<title>商品詳細</title>
 </head>
 <body>
-	<div class="header">
-		<%@include file="header.jsp"%>
+<div class="header">
+<%@include file="header.jsp"%>
+</div>
+<div id="contents">
+<h1>商品詳細画面</h1>
+<s:if test="!productName!=null && !productName.isEmpty()">
+	<s:form action="AddCartAction">
+	<div class="box">
+	<%-- 画面二つに分ける  --%>
+	<div class="2column-container">
+	<%--  右 --%>
+	<div class="right">
+	<%-- 画像挿入  --%>
+		<img src='<s:property value="imageFilePath"/>/<s:property value="imageFileName"/>' class="item-image-box-320"/><br>
 	</div>
-	<div id="maincontainer">
-		<h1>商品詳細画面</h1>
-		<s:form action="AddCartAction">
-		<s:iterator value="productInfoDTO">
-			<tr>
-				<td>
-				<img src='<s:property value="ImagePath"/>/<s:property value="ImageName"/>' width="300px" height="300px"/>
-				</td>
-			</tr>
-			<tr>
-				<td>
-				<s:property value="productInfoDTO.getProductName()" />
-				</td>
-			</tr>
-
-			<table>
-				<tr>
-					<td>商品名</td>
-					<td><s:property value="productInfoDTO.getProductName()" /></td>
-				</tr>
-				<tr>
-					<td>商品名ふりがな</td>
-					<td><s:property value="productInfoDTO.getProductNameKana()" /></td>
-				</tr>
-				<tr>
-					<td>値段</td>
-					<td><s:property value="productInfoDTO.getPrice()" /></td>
-				</tr>
-				<tr>
-					<td>販売個数</td>
-					<td><s:select name="countList" list="%{countList}"/></td>
-				</tr>
-				<tr>
-					<td>販売会社名</td>
-					<td><s:property value="productInfoDTO.getReleaseCompany()" /></td>
-				</tr>
-				<tr>
-					<td>発売年月日</td>
-					<td><s:property value="productInfoDTO.getReleaseDate()" /></td>
-				</tr>
-				<tr>
-					<td>商品詳細情報</td>
-					<td><s:property value="productInfoDTO.getProductDescription()" /></td>
-				</tr>
-			</table>
-			</s:iterator>
-		</s:form>
+	<%--  左 --%>
+	<div class="left">
+	<table class="vertical-list-table-mini">
+		<tr>
+		<th scope="row"><s:label value="商品名"/></th>
+		<td><s:property value="productName"/></td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="商品名ふりがな"/></th>
+		<td><s:property value="productNameKana"/></td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="値段"/></th>
+		<td><s:property value="price"/>円</td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="購入個数"/></th>
+		<td><s:select name="productCount" list="%{countList}"/>個</td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="発売会社名"/></th>
+		<td><s:property value="releaseCompany"/></td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="発売年月日"/></th>
+		<td><s:property value="releaseDate"/></td>
+		</tr>
+		<tr>
+		<th scope="row"><s:label value="商品詳細情報"/></th>
+		<td><s:property value="productDescription"/></td>
+		</tr>
+		</table>
 	</div>
-	<div id = "sub">
-	<h2>関連商品</h2>
-		<s:iterator value = "selectRelativeList">
-		<a href='<s:url action="ProductDetailsAction"><s:param name="productId" value="%{productId}"/>
-					</s:url>'><img src='<s:property value="imageFilePath"/>/<s:property value="imageFileName"/>' width="300px" height="300px"/></a>
+	</div>
+	<s:hidden name="productId" value="%{productId}"/>
+	</div>
+	<div class="submit_btn_box">
+	<s:submit value="カートに追加" class="submit_btn" />
+	</div>
+	</s:form>
+	<s:if test="productList!=null && productList.size()>0">
+		<div class="box">
+			<div class="product-details-recomｍend-box">
+			<h2>関連商品</h2>
+			<s:iterator value="selectRelativeList">
+					<div class="recommend-box">
+					<a href='<s:url action="ProductDetailsAction">
+					<s:param name="productId" value="%{productId}"/>
+					</s:url>'><img src='<s:property value="imageFilePath"/>/<s:property value="imageFileName"/>' class="item-image-box-100"/></a>
 					<s:property value="productName"/><br>
+					</div>
 			</s:iterator>
+			</div>
 		</div>
-
-
+	</s:if>
+</s:if>
+<s:else>
+	<div class="info">
+		商品の詳細情報がありません。
+	</div>
+</s:else>
+</div>
 </body>
 </html>
--- src/com/internousdev/earth/action/ProductDetailsAction.java
+++ src/com/internousdev/earth/action/ProductDetailsAction.java
@@ -11,137 +11,40 @@ import com.internousdev.earth.dto.ProductInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductDetailsAction extends ActionSupport implements SessionAware {
-	private int productId;
-	// private String productName;
-	// private String productNameKana;
-	// private String imageFilePath;
-	// private String imageFileName;
-	// private int price;
-	// private String releaseCompany;
-	// private Date releaseDate;
-	// private String productDescription;
+	private int id;
 	private Map<String, Object> session;
+	private ProductInfoDAO productInfoDAO = new ProductInfoDAO();
+	private ArrayList<ProductInfoDTO> productInfoDTOList = new ArrayList<ProductInfoDTO>();
+	private ProductInfoDTO dto = new ProductInfoDTO();
 	private List<ProductInfoDTO> selectRelativeList;
 	private List<Integer> countList;
 
 	public String execute() {
-		if (session.isEmpty()) {
-			return "sessionTimeout";
-		}
+		String result = ERROR;
+		dto = productInfoDAO.selectByProductId(id);
 
-		// 商品情報取得
-		// DAO,DTOの実体化
-		ProductInfoDAO productInfoDAO = new ProductInfoDAO();
-		ProductInfoDTO productInfoDTO = new ProductInfoDTO();
-		// selectByProductIdメソッド呼び出し、引数int productId
-		// 戻り値をproductInfoDTOに格納
-		productInfoDTO = productInfoDAO.selectByProductId(productId);
+		// 関連商品
+		selectRelativeList = productInfoDAO.selectRelative(dto.getCategoryId());
 
-		// SELECT結果がDTOに格納(set)されるので呼び出し(get)→jspで出来れば無駄なコードは要らない
-		// 商品情報ごとに分ける
-		// 商品ID
-		// productId = productInfoDTO.getProductId();
-		// 商品名
-		// productName = productInfoDTO.getProductName();
-		// 商品名かな
-		// productNameKana = productInfoDTO.getProductNameKana();
-		// 画像ファイルパス
-		// imageFilePath = productInfoDTO.getImageFilePath();
-		// 画像ファイル名
-		// imageFileName = productInfoDTO.getImageFileName();
-		// 価格
-		// price = productInfoDTO.getPrice();
-		// 発売会社
-		// releaseCompany = productInfoDTO.getReleaseCompany();
-		// 発売年月
-		// releaseDate = productInfoDTO.getReleaseDate();
-		// 商品詳細
-		// productDescription = productInfoDTO.getProductDescription();
+		// 商品一覧
+		dto = productInfoDAO.selectByProductId(dto.getCategoryId());
 
-		// 関連情報の取得、引数にint categoryIdを渡す
-		// 戻り値をselectRelativeListに格納→jspでiterator
-		selectRelativeList = productInfoDAO.selectRelative(productInfoDTO.getCategoryId());
-
-		//商品個数
+		// 購入個数のリストを作成
 		countList = new ArrayList<Integer>();
 		for (int i = 1; i <= 5; i++) {
 			countList.add(i);
 		}
 
-		return SUCCESS;
+		return result;
 	}
 
-	public int getProductId() {
-		return productId;
+	public int getId() {
+		return id;
 	}
 
-	public void setProductId(int productId) {
-		this.productId = productId;
+	public void setId(int id) {
+		this.id = id;
 	}
-
-	// public String getProductName() {
-	// return productName;
-	// }
-	//
-	// public void setProductName(String productName) {
-	// this.productName = productName;
-	// }
-	//
-	// public String getProductNameKana() {
-	// return productNameKana;
-	// }
-	//
-	// public void setProductNameKana(String productNameKana) {
-	// this.productNameKana = productNameKana;
-	// }
-	//
-	// public String getImageFilePath() {
-	// return imageFilePath;
-	// }
-	//
-	// public void setImageFilePath(String imageFilePath) {
-	// this.imageFilePath = imageFilePath;
-	// }
-	//
-	// public String getImageFileName() {
-	// return imageFileName;
-	// }
-	//
-	// public void setImageFileName(String imageFileName) {
-	// this.imageFileName = imageFileName;
-	// }
-	//
-	// public int getPrice() {
-	// return price;
-	// }
-	//
-	// public void setPrice(int price) {
-	// this.price = price;
-	// }
-	//
-	// public String getReleaseCompany() {
-	// return releaseCompany;
-	// }
-	//
-	// public void setReleaseCompany(String releaseCompany) {
-	// this.releaseCompany = releaseCompany;
-	// }
-	//
-	// public Date getReleaseDate() {
-	// return releaseDate;
-	// }
-	//
-	// public void setReleaseDate(Date releaseDate) {
-	// this.releaseDate = releaseDate;
-	// }
-	//
-	// public String getProductDescription() {
-	// return productDescription;
-	// }
-	//
-	// public void setProductDescription(String productDescription) {
-	// this.productDescription = productDescription;
-	// }
 
 	public Map<String, Object> getSession() {
 		return session;
@@ -151,12 +54,28 @@ public class ProductDetailsAction extends ActionSupport implements SessionAware 
 		this.session = session;
 	}
 
-	public List<ProductInfoDTO> getSelectRelativeList() {
-		return selectRelativeList;
+	public ProductInfoDAO getProductInfoDAO() {
+		return productInfoDAO;
 	}
 
-	public void setSelectRelativeList(List<ProductInfoDTO> selectRelativeList) {
-		this.selectRelativeList = selectRelativeList;
+	public void setProductInfoDAO(ProductInfoDAO productInfoDAO) {
+		this.productInfoDAO = productInfoDAO;
+	}
+
+	public ArrayList<ProductInfoDTO> getProductInfoDTOList() {
+		return productInfoDTOList;
+	}
+
+	public void setProductInfoDTOList(ArrayList<ProductInfoDTO> productInfoDTOList) {
+		this.productInfoDTOList = productInfoDTOList;
+	}
+
+	public ProductInfoDTO getDto() {
+		return dto;
+	}
+
+	public void setDto(ProductInfoDTO dto) {
+		this.dto = dto;
 	}
 
 	public List<Integer> getCountList() {
@@ -167,4 +86,11 @@ public class ProductDetailsAction extends ActionSupport implements SessionAware 
 		this.countList = countList;
 	}
 
+	public List<ProductInfoDTO> getSelectRelativeList() {
+		return selectRelativeList;
+	}
+
+	public void setSelectRelativeList(List<ProductInfoDTO> selectRelativeList) {
+		this.selectRelativeList = selectRelativeList;
+	}
 }
